c54432ed56f25fdebab9b22b406b2e7acc724664
alfonsomzrtt <alfonsomzrt@gmail.com>
2025-11-11 19:32:55 +0700

    push ke fork, dan mau mencoba hosting di netlify

--- hamburger.js
+++ hamburger.js
@@ -20,7 +20,6 @@ imageUpload.addEventListener("change", (event) => {
 // Hamburger menu untuk tampilan mobile
 const hamburger = document.getElementById("hamburger");
 const navLinks = document.querySelector(".nav-links");
-
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
